5bb5ce09d231aa1652b56a69b94334c06a666bf6
biancamdros <bibilaise@gmail.com>
2023-01-27 19:54:14 -0300

    Adicionando usuários

--- lib/views/user_form.dart
+++ lib/views/user_form.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:listadeusuario/models/user.dart';
+import 'package:listadeusuario/provider/users.dart';
+import 'package:provider/provider.dart';
 
 class UserForm extends StatelessWidget {
+  final _form = GlobalKey<FormState>();
+  final Map<String, String> _formData = {};
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -9,7 +14,19 @@ class UserForm extends StatelessWidget {
         actions: <Widget>[
           IconButton(
               onPressed: () {
-                Navigator.of(context).pop();
+                final isValid = _form.currentState!.validate();
+                if (isValid) {
+                  _form.currentState!.save();
+                  Provider.of<Users>(context, listen: false).put(
+                    User(
+                        id: _formData['id'].toString(), 
+                        name: _formData['name'].toString(), 
+                        email: _formData['email'].toString(), 
+                        avatarUrl: _formData['avatarUrl'].toString(),
+                    ),
+                  );
+                  Navigator.of(context).pop();
+                }
               },
               icon: Icon(Icons.save))
         ],
@@ -17,19 +34,30 @@ class UserForm extends StatelessWidget {
       body: Padding(
         padding: EdgeInsets.all(15),
         child: Form(
-          child: Column(
-            children: <Widget>[
-              TextFormField(
-                decoration:InputDecoration(labelText: 'Nome'),
-              ),
-              TextFormField(
-                decoration:InputDecoration(labelText: 'E-mail'),
-              ),
-              TextFormField(
-                decoration:InputDecoration(labelText: 'URL do Avatar'),
-              ),
-            ]
-          ),
+          key: _form,
+          child: Column(children: <Widget>[
+            TextFormField(
+              decoration: InputDecoration(labelText: 'Nome'),
+              onSaved: (value) => _formData['name'] = value!,
+              validator: (value) {
+                if (value == null || value.trim().isEmpty) {
+                  return "Nome inválido";
+                }
+                if (value.trim().length < 3) {
+                  return "Nome muito pequeno, mínimo exigido são 3 letras.";
+                }
+                return null;
+              },
+            ),
+            TextFormField(
+              decoration: InputDecoration(labelText: 'E-mail'),
+              onSaved: (value) => _formData['email'] = value!,
+            ),
+            TextFormField(
+              decoration: InputDecoration(labelText: 'URL do Avatar'),
+              onSaved: (value) => _formData['avatarUrl'] = value!,
+            ),
+          ]),
         ),
       ),
     );
