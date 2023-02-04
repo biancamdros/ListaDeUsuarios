bf8f9894b09af46317d6a57b9cef7fe0e09c4d9c
biancamdros <bibilaise@gmail.com>
2023-02-03 21:52:43 -0300

    Melhorias no código

--- lib/components/user_tile.dart
+++ lib/components/user_tile.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:listadeusuario/models/user.dart';
+import 'package:listadeusuario/provider/users.dart';
+import 'package:provider/provider.dart';
 
 class UserTile extends StatelessWidget {
   final User user;
@@ -8,28 +10,57 @@ class UserTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final avatar = user.avatarUrl == null || user.avatarUrl.isEmpty
-        ? CircleAvatar(child: Icon(Icons.person, color: Colors.white,))
+        ? CircleAvatar(
+            child: Icon(
+            Icons.person,
+            color: Colors.white,
+          ))
         : CircleAvatar(backgroundImage: NetworkImage(user.avatarUrl));
     return ListTile(
       leading: avatar,
       title: Text(user.name),
-      subtitle: Text(user.email), 
+      subtitle: Text(user.email),
       trailing: Container(
         width: 100,
         child: Row(
-          children:<Widget> [
+          children: <Widget>[
             IconButton(
               color: Colors.orange,
-              onPressed: (){}, 
+              onPressed: () {},
               icon: Icon(Icons.edit),
             ),
             IconButton(
               color: Colors.red,
-              onPressed: (){}, 
+              onPressed: () {
+                showDialog(
+                    context: context,
+                    builder: (context) => AlertDialog(
+                          title: Text('Excluir usuário'),
+                          content: Text('Tem certeza?'),
+                          actions: <Widget>[
+                            ElevatedButton(
+                              child: Text('Não'),
+                              onPressed: () {
+                                Navigator.of(context).pop();
+                              },
+                            ),
+                            ElevatedButton(
+                              child: Text('Sim'),
+                              onPressed: () => Navigator.of(context).pop(true),
+                            ),
+                          ],
+                        )).then((confirmed) => {
+                      if (confirmed)
+                        {
+                          Provider.of<Users>(context, listen: false).remove(user),
+                        }
+                    });
+              },
               icon: Icon(Icons.delete),
             ),
           ],
         ),
-      ),);
+      ),
+    );
   }
 }
